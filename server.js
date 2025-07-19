f2bd69f90d2c2736890c44a2eb3fe057f295233d
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-19 21:46:52 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// Load environment variables
+require('dotenv').config();
+
+// Import packages
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Initialize app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Port from .env or default
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost: ${PORT}`)
+})
