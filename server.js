626b2cdf890d18264647abfa25f5e6a10b85172c
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-19 21:58:40 +0100

    Connect Mongoose to the Database

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.use(bodyParser.json());
 // Port from .env or default
 const PORT = process.env.PORT || 5000;
 
+// Connect Mongoose to Database
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost: ${PORT}`)
